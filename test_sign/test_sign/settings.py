9df831835eb7f9a6ac68490dba8802b70b533fc1
zhangbo <zhangbo@zhangbodeMacBook-Pro.local>
2019-03-28 02:29:28 +0800

    2019-03-28 提交

--- test_sign/test_sign/settings.py
+++ test_sign/test_sign/settings.py
@@ -91,7 +91,7 @@ WSGI_APPLICATION = 'test_sign.wsgi.application'
 #     'default': {
 #         'ENGINE': 'django.db.backends.mysql',
 #         'OPTIONS': {
-#             'read_default_file': os.path.join(BASE_DIR,'my.cnf'),
+#             'read_default_file': os.path.join(BASE_DIR+'/my.cnf'),
 #         },
 #     }
 # }
@@ -104,8 +104,8 @@ DATABASES = {
                         'PORT':'3306',
                         'NAME':'sign_db',
                         'USER':'root',
-                        # 'PASSWORD':'1234qwer',
-                        'PASSWORD':'123456',
+                        'PASSWORD':'1234qwer',
+                        # 'PASSWORD':'123456',
                         'OPTIONS':{
                                             'init_command':"SET sql_mode='STRICT_TRANS_TABLES'",
                                             },
